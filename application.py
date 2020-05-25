93cbdbfacecebdaf282b88c923417f1d9a0057a9
Shirono Yuta <yshirono@s.h.k.u-tokyo.ac.jp>
2020-05-25 18:26:13 +0900

    lib test2

--- application.py
+++ application.py
@@ -1,4 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify, json, make_response
+from io import BytesIO
+import urllib
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import csv
+import definition
+
 app = Flask(__name__)
 
 @app.route('/')
