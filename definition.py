6b30f547666ccc5f40591e100218b7a711cb0ffc
Shirono Yuta <yshirono@s.h.k.u-tokyo.ac.jp>
2020-05-25 19:23:15 +0900

    add new file3

--- definition.py
+++ definition.py
@@ -1,18 +1,15 @@
 from flask import Flask, render_template, request, jsonify, json, make_response
 from io import BytesIO
-import urllib
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import csv
-
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import os
 import os.path
 import pandas as pd
 import codecs
-import MyModules 
-
+import MyModules
 import definition
 
 def AnnualCost(algo,term,aitaiM,aitaiN):#相対はリスト
