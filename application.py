5ddb6ef5cc73961993aeaba2dfd1221c66777c63
Shirono Yuta <yshirono@s.h.k.u-tokyo.ac.jp>
2020-05-25 20:30:16 +0900

    add new file4

--- application.py
+++ application.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, request, jsonify, json, make_response
 from io import BytesIO
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 import csv
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import os
 import os.path
+import pandas as pd
 import codecs
-
+import MyModules
+import definition
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
