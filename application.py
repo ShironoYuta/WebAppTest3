f77262841eaf1d70df405d78e4074998b4224ff7
Shirono Yuta <yshirono@s.h.k.u-tokyo.ac.jp>
2020-05-31 19:33:38 +0900

    test library3

--- application.py
+++ application.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request, jsonify, json, make_response
 import numpy
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "hello world!"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
